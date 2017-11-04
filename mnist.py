a6bd7ad9963408b6f18919d49ed3144b65feeee5
amitrajitbose <amitrajitbose9@gmail.com>
2017-11-04 22:45:35 +0530

    Updated array size of dataset

--- mnist.py
+++ mnist.py
@@ -6,11 +6,14 @@ import numpy as np
 from sklearn.datasets import fetch_mldata
 from sklearn.neural_network import MLPClassifier
 
-mnist = fetch_mldata("MNIST original")
+#mnist = fetch_mldata("MNIST original")
+mnist = fetch_mldata('iris')
+mnist.target.shape
+mnist.data.shape
 # rescale the data, use the traditional train/test split
 X, y = mnist.data / 255., mnist.target
-X_train, X_test = X[:60000], X[60000:]
-y_train, y_test = y[:60000], y[60000:]
+X_train, X_test = X[0:141], X[141:151]
+y_train, y_test = y[0:141], y[141:151]
 
 # mlp = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=400, alpha=1e-4,
 #                     solver='sgd', verbose=10, tol=1e-4, random_state=1)
@@ -21,7 +24,7 @@ mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10, alpha=1e-4,
 mlp.fit(X_train, y_train)
 print("Training set score: %f" % mlp.score(X_train, y_train))
 print("Test set score: %f" % mlp.score(X_test, y_test))
-time.sleep(5)
+#time.sleep(5)
 fig, axes = plt.subplots(4, 4)
 # use global min / max to ensure all weights are shown on the same scale
 vmin, vmax = mlp.coefs_[0].min(), mlp.coefs_[0].max()
